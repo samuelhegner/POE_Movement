1d52a3b793a1435818c1c7e60f9de8b55f0c48c8
Samuel Hegner <C16439262@mydit.ie>
2020-08-10 14:19:35 +0100

    Comments and ToDo s for the movements code (Animation is next)

--- PathOfExile_Movement/Assets/PlayerMovement.cs
+++ PathOfExile_Movement/Assets/PlayerMovement.cs
@@ -18,12 +18,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        //ToDo holding down mouse button to periodically set the new location
         if (Input.GetMouseButtonDown(0))
         {
             RaycastToMouse();
         }
     }
 
+    /// <summary>
+    /// Shoot raycast towards the mouse cursor
+    /// if possible, set a new location to move towards
+    /// </summary>
     void RaycastToMouse()
     {
         RaycastHit hit;
@@ -45,6 +50,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Check if the selected point is possible to navigate to
+    /// </summary>
+    /// <param name="hit"> The ray cast hit to compare layers with </param>
+    /// <returns></returns>
     bool CheckNavigableGround(RaycastHit hit)
     {
         if (navigableMask == (navigableMask | (1 << hit.transform.gameObject.layer)))
@@ -56,9 +66,18 @@ public class PlayerMovement : MonoBehaviour
             return false;
         }
     }
-
+    
+    /// <summary>
+    /// Set the location for navigation
+    /// If the point is close and the path to it is short move straight to the point
+    /// if the path is long, select a point in a straight line closest to the location
+    /// </summary>
+    /// <param name="worldSpaceLocation"> Location to move to in worldspace </param>
     void SetNewLocation(Vector3 worldSpaceLocation)
     {
+        //ToDo check distance to point for more simplistic close movement
+        
+        //sets the agents destination point
         agent.SetDestination(worldSpaceLocation);
     }
 }
