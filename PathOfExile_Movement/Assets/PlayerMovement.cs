f859c1cd3c12ce3759c2c3a37119cc795f3b3d25
Samuel Hegner <C16439262@mydit.ie>
2020-08-10 13:59:00 +0100

    Navigation Mesh Start and Player movement start

---
+++ PathOfExile_Movement/Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+
+    [SerializeField] private LayerMask navigableMask;
+
+    [SerializeField] private NavMeshAgent agent;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastToMouse();
+        }
+    }
+
+    void RaycastToMouse()
+    {
+        RaycastHit hit;
+       
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit)) {
+            if(CheckNavigableGround(hit)){
+                SetNewLocation(hit.point);
+            }
+            else
+            {
+                Debug.Log(" was hit but is not navigable");
+            }
+        }
+        else
+        {
+            Debug.Log("Nothing was hit");
+        }
+    }
+
+    bool CheckNavigableGround(RaycastHit hit)
+    {
+        if (hit.transform.gameObject.layer == navigableMask)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void SetNewLocation(Vector3 worldSpaceLocation)
+    {
+        agent.SetDestination(worldSpaceLocation);
+    }
+}
